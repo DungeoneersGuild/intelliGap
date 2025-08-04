4a5fe927bb5ea39b301074e0698856e070611806
DungeoneersGuild <dungeoneerswanted@gmail.com>
2025-08-04 08:25:28 +0200

    feat: added additional considerations to Governor.sol

--- contracts/MyGovernor.sol
+++ contracts/MyGovernor.sol
@@ -1,27 +1,68 @@
-// SPDX-License-Identifier: AGPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/governance/Governor.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import "@openzeppelin/contracts/governance/TimelockController.sol";
 
-contract MyGovernor is Governor, GovernorSettings, GovernorVotes, GovernorCountingSimple {
-    constructor(IVotes _token)
+contract MyGovernor is Governor, GovernorSettings, GovernorVotes, GovernorCountingSimple, GovernorTimelockControl {
+
+    constructor(IVotes _token, TimelockController _timelock)
         Governor("MyGovernor")
-        GovernorSettings(1 /* delay */, 50400 /* voting period */, 0 /* quorum */)
+        GovernorSettings(1 days, 3 days, 0) // votingDelay: 1 day, votingPeriod: 3 days
         GovernorVotes(_token)
+        GovernorTimelockControl(_timelock)
     {}
 
-    function votingDelay() public view override returns (uint256) {
-        return super.votingDelay();
-    }
-
-    function votingPeriod() public view override returns (uint256) {
-        return super.votingPeriod();
-    }
-
-    function quorum(uint256 blockNumber) public pure override returns (uint256) {
+    // Override required by Solidity
+    function quorum(uint256) public pure override returns (uint256) {
         return 0;
+    }
+
+    function state(uint256 proposalId)
+        public
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (ProposalState)
+    {
+        return super.state(proposalId);
+    }
+
+    function _execute(
+        uint256 proposalId,
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    )
+        internal
+        override(Governor, GovernorTimelockControl)
+    {
+        super._execute(proposalId, targets, values, calldatas, descriptionHash);
+    }
+
+    function _cancel(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    )
+        internal
+        override(Governor, GovernorTimelockControl)
+        returns (uint256)
+    {
+        return super._cancel(targets, values, calldatas, descriptionHash);
+    }
+
+    function _executor()
+        internal
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (address)
+    {
+        return super._executor();
     }
 }
